36c59f25ae48d1524e1f27d15feb38a18cb73bd1
Nino Kristesiashvili <Nino Kristesiashvili>
2023-02-23 16:14:08 +0400

    added webcam styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'easyShopper';
+  trigger$: Subject<void> = new Subject<void>();
+  imagePath!: SafeResourceUrl;
+  constructor(private _sanitizer: DomSanitizer) {}
+
+  saveImage(img: any) {
+    this.imagePath = this._sanitizer.bypassSecurityTrustResourceUrl(
+      img._imageAsDataUrl
+    );
+  }
+
+  captureImg() {
+    this.trigger$.next();
+  }
 }
