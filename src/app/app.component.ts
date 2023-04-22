1ae40ea2a50e9717568efb54055de5762b8d6f8a
Nino Kristesiashvili <Nino Kristesiashvili>
2023-04-22 22:04:36 +0400

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Observable, Subject } from 'rxjs';
 
@@ -8,13 +14,22 @@ import { Observable, Subject } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'easyShopper';
   trigger$: Subject<void> = new Subject<void>();
   imagePath!: SafeResourceUrl;
   detection: any;
-  constructor(private _sanitizer: DomSanitizer, private http: HttpClient) {}
-
+  weight: any;
+  constructor(private http: HttpClient, private elRef: ElementRef) {}
+  prices: any = {
+    person: 2,
+    apple: 3.2,
+    banana: 4.2,
+    orange: 5,
+  };
+  ngAfterViewInit() {
+    console.log(this.elRef.nativeElement.querySelector('.weight'));
+  }
   saveImage(img: any) {
     const imgBase64 = img._imageAsDataUrl.split(',')[1];
 
@@ -22,10 +37,13 @@ export class AppComponent {
       .post('http://127.0.0.1:5000/', { data: imgBase64 })
       .subscribe((detection) => {
         this.detection = detection;
+        console.log(detection);
       });
   }
 
   captureImg() {
+    console.log('here');
+
     this.trigger$.next();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { WebcamModule } from 'ngx-webcam';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, WebcamModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    WebcamModule,
+    FormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
